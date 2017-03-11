70c49405cb3500956299a655096253953347ddf2
Erik Madsen <beatmadsen@gmail.com>
2017-03-11 02:06:39 +0100

    EM: Wip lexer interface

--- spec/factories/lexer/simple.rb
+++ spec/factories/lexer/simple.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :lexer, class: Tell::Interpreter::Lexer::Simple do
-
+    text ''
+    initalize_with { new(text) }
   end
 
 end
--- spec/tell/interpreter/lexer/simple_spec.rb
+++ spec/tell/interpreter/lexer/simple_spec.rb
@@ -1,9 +1,54 @@
 require 'spec_helper'
 
 RSpec.describe Tell::Interpreter::Lexer::Simple do
-  let(:lexer) { build(:lexer) }
 
-  it 'can be created' do
-    expect(lexer).not_to be nil
+
+  context 'with a hello world program' do
+
+    let(:program) do
+      rel = '../../../../support/helloworld.tell'
+      File.read(File.expand_path(rel, __FILE__))
+    end
+    let(:lexer) { build(:lexer, text: program) }
+
+    it 'should find tokens' do
+      # lexer wraps itself in enumerator
+      tokens = lexer.tokens.entries
+      expect(tokens).to match_array [
+        :namespace,
+        [:name, 'com.madsen'],
+        :linebreak,
+        :import,
+        [:name, 'tell.io.Io'],
+        :linebreak,
+        :main_class,
+        [:name, 'HelloWorld'],
+        :linebreak,
+        :pm,
+        [:name, 'run'],
+        :lpar,
+        :asterix,
+        :name,
+        :rpar,
+        :linebreak,
+        [:name, 'io'],
+        :equals,
+        [:name, 'Io'],
+        :dot,
+        [:name, 'new'],
+        :linebreak,
+        [:name, 'io'],
+        :dot,
+        [:name, 'puts'],
+        :lpar,
+        :apo,
+        [:name, 'Hello World'],
+        :apo,
+        :rpar,
+        :comment,
+
+
+      ]
+    end
   end
 end
