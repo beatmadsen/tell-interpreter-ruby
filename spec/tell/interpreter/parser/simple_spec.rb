2dad9c981ffdc4c8e16d0706fc664df506cf3db0
Erik Madsen <beatmadsen@gmail.com>
2017-03-18 23:48:01 +0100

    EM: failing test, parser

---
+++ spec/tell/interpreter/parser/simple_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Tell::Interpreter::Parser::Simple do
+  context 'with single token input' do
+    let(:parser) { build(:parser) }
+
+    it 'complains about missing main class' do
+      raise 'not implementated'
+    end
+  end
+end
